cd44025c26bad0516c31886b911003b1e4d5cac3
tkharseh <tariq.kharseh@mail.utoronto.ca>
2021-11-06 14:03:13 -0400

    implemented images scraper, key concepts

---
+++ backend/images.py
@@ -0,0 +1,21 @@
+from serpapi import GoogleSearch
+from config import serp_key
+
+def get_images(keyword):
+    '''
+    Scrapes the top 3 images for this keyword
+    '''
+
+    params = {
+        "q": keyword,
+        "tbm": "isch",
+        "ijn": "0",
+        "api_key": serp_key
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    images_results = results['images_results']
+    top_three_links = [images_results[0]['thumbnail'], images_results[1]['thumbnail'], images_results[2]['thumbnail']]
+
+    return top_three_links
--- backend/key_concepts.py
+++ backend/key_concepts.py
@@ -1,3 +1,4 @@
+
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, ConceptsOptions
